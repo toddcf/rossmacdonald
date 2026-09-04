d043400f20fb9d0e2a20328e295319019e4a27f9
Todd Croak-Falen <toddcf@gmail.com>
2026-09-04 16:59:51 -0700

    Initial commit

---
+++ assets/js/titles.js
@@ -0,0 +1,31 @@
+const titles = [
+  {
+    editions: [
+      {
+        authors: [
+          {
+            name: {
+              first: 'John',
+              last: 'Macdonald',
+            },
+          },
+        ],
+        characters: {
+          main: {
+            name: {
+              first: 'Lew',
+              last: 'Archer',
+            },
+          },
+        },
+        date: new Date('April 11, 1949'),
+        publisher: {
+          city: 'New York',
+        },
+        title: {
+          official: 'The Moving Target',
+        },
+      },
+    ],
+  },
+];
